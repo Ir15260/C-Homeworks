4b5ac2c5bf3abbd499af961900428414620f04a9
Ir15260 <149706932+Ir15260@users.noreply.github.com>
2024-02-29 21:36:32 +0100

    Add files via upload

---
+++ Class 2 Homework/QinshiftAcademy.HomeworkClass2.Excercise1/HomeworkClass2.Excercise2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+//Create a new console application called “SwapNumbers”. Input 2 numbers from the console and
+//then swap the values of the variables so that the first variable has the second value and the second variable the first value. Please find example below:
+//Test Data:
+//Input the First Number: 5
+//Input the Second Number: 8
+//Expected Output:
+//After Swapping:
+//First Number: 8
+//Second Number: 5
+
+
+
+
+double firstNumber = 5;
+double secondNumber = 8;
+
+firstNumber = firstNumber + secondNumber;
+secondNumber = firstNumber - secondNumber;
+firstNumber = firstNumber - secondNumber;
+
+Console.WriteLine($"This is the First Number: {firstNumber} and this is the Second Number: {secondNumber}");
+
+Console.WriteLine("Imput the First Number");
+string inputFirstNumber = Console.ReadLine();
+double convertedFirstNumber = Convert.ToDouble(inputFirstNumber);
+
+Console.WriteLine("Imput the Second Number");
+string inputSecondNumber = Console.ReadLine();
+double convertedSecondNumber = Convert.ToDouble(inputSecondNumber);
+
+convertedFirstNumber = convertedFirstNumber + convertedSecondNumber;
+convertedSecondNumber = convertedFirstNumber - convertedSecondNumber;
+convertedFirstNumber = convertedFirstNumber - convertedSecondNumber;
+
+Console.WriteLine($"This is the First Number: {convertedFirstNumber} and this is the Second Number: {convertedSecondNumber}");
---
+++ Class 2 Homework/QinshiftAcademy.HomeworkClass2.Excercise1/QinshiftAcademy.HomeworkClass2.Excercise1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+//Create a new console application called “AverageNumber” that takes four numbers as input to calculate and print the average.
+
+//Test Data:
+//Enter the First number: 10
+//Enter the Second number: 15
+//Enter the third number: 20
+//Enter the fourth number: 30
+//Expected Output:
+//The average of 10, 15, 20 and 30 is: 18
+
+double firstNumber = 10;
+double secondNumber = 15;
+double thirdNumber = 20;
+double fourthNumber = 30;
+
+double averageNumber = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+
+Console.WriteLine($"The Average Number out of the 4 is : {averageNumber}");
+
+Console.WriteLine("Imput the First Number");
+string inputFirstNumber = Console.ReadLine();
+double convertedFirstNumber = Convert.ToDouble(inputFirstNumber);
+
+Console.WriteLine("Imput the Second Number");
+string inputSecondNumber = Console.ReadLine();
+double convertedSecondNumber = Convert.ToDouble(inputSecondNumber);
+
+Console.WriteLine("Imput the Third Number");
+string inputThirdNumber = Console.ReadLine();
+double convertedThirdNumber = Convert.ToDouble(inputThirdNumber);
+
+Console.WriteLine("Imput the Forth Number");
+string inputForthNumber = Console.ReadLine();
+double convertedForthNumber = Convert.ToDouble(inputForthNumber);
+
+double convertedResultAverageNumber = (convertedFirstNumber + convertedSecondNumber + convertedThirdNumber + convertedForthNumber) / 4;
+
+Console.WriteLine($"The Converted Average Number out of the 4 is : {convertedResultAverageNumber}");
